685199553b05d1e44b767f893a801d3abf570064
Gleb <gleb.nutels@softec.cz>
2024-08-15 19:56:33 +0200

    delete btn + alert

--- src/app/measurements/measurements.component.ts
+++ src/app/measurements/measurements.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { ChangeDetectorRef } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-measurements',
@@ -17,7 +20,7 @@ export class MeasurementsComponent implements OnInit {
   measurements: any[] = [];
   id: any;
 
-  constructor(private cdr: ChangeDetectorRef, private fb: FormBuilder, private route: ActivatedRoute, private http: HttpClient) { }  
+  constructor(private router: Router, private cdr: ChangeDetectorRef, private fb: FormBuilder, private route: ActivatedRoute, private http: HttpClient) { }  
   
   ngOnInit(): void {
 	const id = this.route.snapshot.queryParamMap.get('id');
@@ -64,9 +67,13 @@ export class MeasurementsComponent implements OnInit {
           heartRate: response.heartRate
         });
 		console.log('generator ok');
+		setTimeout(() => {
+          this.saveMeasurement();
+        }, 2000);
       }, error => {
         console.error('Error measuring data', error);
       });
+	
   }
 
   saveMeasurement(): void {
@@ -81,9 +88,63 @@ export class MeasurementsComponent implements OnInit {
         }, error => {
           console.error('Error submitting measurement data', error);
         }); 
-      this.measurementsForm.reset(); // Очистка формы после сохранения
+      //this.measurementsForm.reset(); // Очистка формы после сохранения
     } else {
       console.log('Form is invalid');
     }
   }
+  
+  cleanAll(): void {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: 'This action clears all history and cannot be undone!',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Yes, clear',
+    cancelButtonText: 'Cancel'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const formData = this.measurementsForm.value;
+
+      this.http.delete('http://localhost:8088/api/v1/measurements/' + this.id, formData)
+        .subscribe(data => {
+          console.log('Success all clean');
+          this.getmeasure();
+          this.cdr.detectChanges();
+        }, error => {
+          console.error('Error clean measurement data', error);
+        });
+    }
+  });
+  }
+  
+  cleanPerson(): void {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: 'This action removes you profile and cannot be undone!',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Yes, remove',
+    cancelButtonText: 'Cancel'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const formData = this.measurementsForm.value;
+
+      this.http.delete('http://localhost:8088/api/v1/persons/person/' + this.id, formData)
+        .subscribe(data => {
+          console.log('Success profile removed');
+          
+        }, error => {
+          console.error('Error remove profile', error);
+        });
+	  this.router.navigate(['/form']);
+    }
+  });
+  }
+
+
 }
